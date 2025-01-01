2ee78b0791aba151fba092f4631a87d06b0c47ab
Vojtech Samek <vojtech.samek@email.cz>
2025-01-01 23:03:34 +0100

    Added switcher for GUI mode

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ namespace NamesControl
 {
     public class Program
     {
+        // MAUI GUI is ran if 'mauiApp' is true, console GUI for developing otherwise
+        public static bool mauiApp = false;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Here will run NamesControl application.");
