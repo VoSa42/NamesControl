eb869aaa3b52a3a4002d592a080bbf3cd5743a54
Vojtech Samek <vojtech.samek@email.cz>
2025-01-04 22:21:46 +0100

    Simple GUI designed

--- NamesControlClient/NCForm.Designer.cs
+++ NamesControlClient/NCForm.Designer.cs
@@ -29,32 +29,133 @@
         private void InitializeComponent()
         {
             SenMessage = new Button();
+            dataGridView1 = new DataGridView();
+            FstName = new DataGridViewButtonColumn();
+            SndName = new DataGridViewTextBoxColumn();
+            Add = new Button();
+            Remove = new Button();
+            Edit = new Button();
+            FefreshTable = new Button();
+            ManiPanel = new Panel();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ManiPanel.SuspendLayout();
             SuspendLayout();
             // 
             // SenMessage
             // 
-            SenMessage.Location = new Point(203, 127);
+            SenMessage.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            SenMessage.Location = new Point(419, 12);
             SenMessage.Name = "SenMessage";
-            SenMessage.Size = new Size(124, 29);
+            SenMessage.Size = new Size(125, 29);
             SenMessage.TabIndex = 0;
             SenMessage.Text = "Send Message";
             SenMessage.UseVisualStyleBackColor = true;
             SenMessage.Click += SenMessage_Click;
             // 
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeight = 29;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { FstName, SndName });
+            dataGridView1.Location = new Point(12, 47);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(663, 289);
+            dataGridView1.TabIndex = 1;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // FstName
+            // 
+            FstName.HeaderText = "First Name";
+            FstName.MinimumWidth = 6;
+            FstName.Name = "FstName";
+            FstName.UseColumnTextForButtonValue = true;
+            // 
+            // SndName
+            // 
+            SndName.HeaderText = "Second Name";
+            SndName.MinimumWidth = 6;
+            SndName.Name = "SndName";
+            // 
+            // Add
+            // 
+            Add.Location = new Point(12, 12);
+            Add.Name = "Add";
+            Add.Size = new Size(125, 29);
+            Add.TabIndex = 2;
+            Add.Text = "Add new name";
+            Add.UseVisualStyleBackColor = true;
+            // 
+            // Remove
+            // 
+            Remove.Location = new Point(143, 12);
+            Remove.Name = "Remove";
+            Remove.Size = new Size(125, 29);
+            Remove.TabIndex = 3;
+            Remove.Text = "Remove Name";
+            Remove.UseVisualStyleBackColor = true;
+            // 
+            // Edit
+            // 
+            Edit.Location = new Point(274, 12);
+            Edit.Name = "Edit";
+            Edit.Size = new Size(125, 29);
+            Edit.TabIndex = 4;
+            Edit.Text = "Edit Name";
+            Edit.UseVisualStyleBackColor = true;
+            // 
+            // FefreshTable
+            // 
+            FefreshTable.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            FefreshTable.Location = new Point(550, 12);
+            FefreshTable.Name = "FefreshTable";
+            FefreshTable.Size = new Size(125, 29);
+            FefreshTable.TabIndex = 5;
+            FefreshTable.Text = "Refresh table";
+            FefreshTable.UseVisualStyleBackColor = true;
+            // 
+            // ManiPanel
+            // 
+            ManiPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            ManiPanel.Controls.Add(dataGridView1);
+            ManiPanel.Location = new Point(0, 0);
+            ManiPanel.Name = "ManiPanel";
+            ManiPanel.Size = new Size(687, 348);
+            ManiPanel.TabIndex = 6;
+            // 
             // NCForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            AutoScroll = true;
+            ClientSize = new Size(687, 348);
+            Controls.Add(FefreshTable);
+            Controls.Add(Edit);
+            Controls.Add(Remove);
+            Controls.Add(Add);
             Controls.Add(SenMessage);
+            Controls.Add(ManiPanel);
+            MdiChildrenMinimizedAnchorBottom = false;
+            MinimumSize = new Size(560, 140);
             Name = "NCForm";
-            StartPosition = FormStartPosition.CenterScreen;
             Text = "Names Control";
+            WindowState = FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ManiPanel.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Button SenMessage;
+        private DataGridView dataGridView1;
+        private DataGridViewButtonColumn FstName;
+        private DataGridViewTextBoxColumn SndName;
+        private Button Add;
+        private Button Remove;
+        private Button Edit;
+        private Button FefreshTable;
+        private Panel ManiPanel;
     }
 }
--- NamesControlClient/NCForm.cs
+++ NamesControlClient/NCForm.cs
@@ -11,5 +11,10 @@ namespace NamesControlClient
         {
             GuiEventHandler.TestClickHandler();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
