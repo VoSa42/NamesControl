876734d621f92b3ff78c79ec9ff79fb019decc9a
Vojtech Samek <vojtech.samek@email.cz>
2025-01-04 11:54:05 +0100

    Addedd NamesControl lib

---
+++ PacketManager/PacketManager.cs
@@ -0,0 +1,16 @@
+﻿namespace NamesControlLib
+{
+    public static class PacketManager
+    {
+        public static byte[] WrapCommand(string command)
+        {
+            // dummy code
+            return new byte[42];
+        }
+        public static string UnwrapCommand(byte[] message)
+        {
+            // dummy code
+            return "";
+        }
+    }
+}
