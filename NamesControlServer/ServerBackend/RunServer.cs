923105ee812d2ac829edb81adb54d34518170d50
Vojtech Samek <vojtech.samek@email.cz>
2025-01-04 09:17:12 +0100

    Added server listener - server

--- NamesControlServer/ServerBackend/RunServer.cs
+++ NamesControlServer/ServerBackend/RunServer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,10 @@ namespace NamesControlServer.ServerBackend
     {
         public static void Run()
         {
+            IPEndPoint ipep = new IPEndPoint(ProgramServer.ServerIPAddress, ProgramServer.port);
+            TcpListener listener = new TcpListener(ipep);
+            listener.Start();
+
             while (true)
             {
                 string command = "Dummy";
