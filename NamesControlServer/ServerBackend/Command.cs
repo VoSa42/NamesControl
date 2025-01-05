67ddb0ca4faf480335f87c2a41aed47a2a9bd8e6
Vojtech Samek <vojtech.samek@email.cz>
2025-01-05 15:22:05 +0100

    Added Command structure - server

---
+++ NamesControlServer/ServerBackend/Command.cs
@@ -0,0 +1,27 @@
+﻿namespace NamesControlServer.ServerBackend
+{
+    internal struct Command
+    {
+        string command { get; set; }
+        string? id { get; set; }
+        string? firstName { get; set; }
+        string? secondName { get; set; }
+
+        public Command()
+        {
+            command = "";
+            id = null;
+            firstName = null;
+            secondName = null;
+        }
+
+        public Command(string fullCommand)
+        {
+            var parsedCommand = fullCommand.Split('-');
+            command = parsedCommand[0];
+            id = parsedCommand[1];
+            firstName = parsedCommand[2];
+            secondName = parsedCommand[3];
+        }
+    }
+}
