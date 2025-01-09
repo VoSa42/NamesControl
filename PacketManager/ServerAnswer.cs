d51665fd367565f9cd7012db366a7533d5e1f76d
Vojtech Samek <vojtech.samek@email.cz>
2025-01-09 22:10:56 +0100

    Added ServerAnswer structure - NCLib

---
+++ PacketManager/ServerAnswer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamesControlLib
+{
+    public struct ServerAnswer
+    {
+        public string? ErrorMessage { get; } = null;
+        public (int, string, string)[] Grid { get; } = { };
+
+        public ServerAnswer() { }
+    }
+}
