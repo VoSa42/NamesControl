b405fae7da1573c90a972131604de24fec1d80cd
Vojtech Samek <vojtech.samek@email.cz>
2025-01-09 16:49:32 +0100

    Added 'refresh' command to the logic

--- NamesControlServer/ServerBackend/CommandHandler.cs
+++ NamesControlServer/ServerBackend/CommandHandler.cs
@@ -30,6 +30,9 @@ namespace NamesControlServer.ServerBackend
                 case CommandType.Edit:
                     comHandler = new(EditHandler);
                     break;
+                case CommandType.Refresh:
+                    comHandler = new(RefreshHandler);
+                    break;
                 default:
                     comHandler = new(IncorrectInputHandler);
                     break;
@@ -56,6 +59,11 @@ namespace NamesControlServer.ServerBackend
         private static string EditHandler(Command com)
         {
             return "Edit";
+        }
+
+        private static string RefreshHandler(Command com)
+        {
+            return "Refresh";
         }
 
         private static string IncorrectInputHandler(Command com)
--- PacketManager/CommandType.cs
+++ PacketManager/CommandType.cs
@@ -11,6 +11,7 @@ namespace NamesControlLib
         None,
         Add,
         Remove,
-        Edit
+        Edit,
+        Refresh
     }
 }
