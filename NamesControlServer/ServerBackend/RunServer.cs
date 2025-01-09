1a16bee7ba47978c09abedd1dfcf675c7979564a
Vojtech Samek <vojtech.samek@email.cz>
2025-01-09 11:12:04 +0100

    Connection to database opened - server

--- NamesControlServer/ServerBackend/RunServer.cs
+++ NamesControlServer/ServerBackend/RunServer.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 using NamesControlLib;
 
@@ -14,6 +15,10 @@ namespace NamesControlServer.ServerBackend
     {
         public static void Run()
         {
+            Console.WriteLine("run server");
+
+            ConnectDatabase();
+
             // Set up and activate listener to wait and recieve message from client
             IPEndPoint ipep = new IPEndPoint(ServerMetadata.ServerIPAddress, ServerMetadata.Port);
             TcpListener listener = new TcpListener(ipep);
@@ -34,6 +39,49 @@ namespace NamesControlServer.ServerBackend
                 Console.WriteLine(command);
                 string answer = CommandHandler.CommandExecutor(command);
                 Console.WriteLine($"Answer: {answer}");
+            }
+        }
+
+        public static void ConnectDatabase()
+        {
+            Console.WriteLine("connecting to database");
+
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;" +
+                "AttachDbFilename=C:\\Users\\Admin\\Documents\\prace\\eprin\\pripadovka\\NamesControl\\NamesControlServer\\Database\\NamesDat.mdf;" +
+                "Integrated Security=True;" +
+                "Connect Timeout=30";
+
+            Console.WriteLine("connection string");
+
+            /*string query = "CREATE TABLE [dbo].[Names] (" +
+                "    [Id]         INT           IDENTITY (1, 1) NOT NULL," +
+                "    [FirstName]  NVARCHAR (50) NOT NULL," +
+                "    [SecondName] NVARCHAR (50) NOT NULL," +
+                "    PRIMARY KEY CLUSTERED ([Id] ASC)" +
+                ");";*/
+
+            string query = "INSERT INTO Names (FirstName, SecondName) VALUES ('John', 'Doe');";
+
+            Console.WriteLine("query");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("before openning");
+
+                connection.Open();
+
+                Console.WriteLine("connection opened");
+
+                /*using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader["Column"].ToString());
+                        }
+                    }
+                }*/
             }
         }
     }
