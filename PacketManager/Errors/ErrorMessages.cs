7377b328f99924569acac92996e841a84ca672eb
Vojtech Samek <vojtech.samek@email.cz>
2025-01-13 01:12:36 +0100

    Added Errors components - NCLib

---
+++ PacketManager/Errors/ErrorMessages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamesControlClient.Errors
+{
+    using ErrorList = List<Tuple<ErrorType, string>>;
+
+    public static class ErrorMessages
+    {
+        private static ErrorList EList { get; } = [
+            new(ErrorType.ServerNotResponse, ErrorType.ServerNotResponse.ToString() + ": Server is not responding at the moment pleas try it later."),
+            new(ErrorType.RecordNotExist, ErrorType.RecordNotExist.ToString() + ": Selected record does not exist in the database already."),
+            new(ErrorType.InvalidQuery, ErrorType.InvalidQuery.ToString() + ": SQL query was incorrect."),
+            ];
+
+        public static string? GetErrorMessage(ErrorType errorType)
+        {
+            foreach (var error in EList)
+            {
+                if (error.Item1 == errorType) { return error.Item2; }
+            }
+            return null;
+        }
+    }
+}
---
+++ PacketManager/Errors/ErrorType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamesControlClient.Errors
+{
+    public enum ErrorType
+    {
+        ServerNotResponse,
+        RecordNotExist,
+        InvalidQuery
+    }
+}
